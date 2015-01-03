c8c85eb7948a9f9670c5f8a5654acc5812e6d002
Chris Wand <chris.wand@gmail.com>
2015-01-03 16:09:42 -0500

    Coveralls and hound

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -51,4 +51,7 @@ RSpec.configure do |config|
   # The different available types are documented in the features, such as in
   # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
+
+  require 'coveralls'
+  Coveralls.wear!('rails')
 end
