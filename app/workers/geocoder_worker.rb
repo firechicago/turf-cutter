39882010406d7c5f83c3692376305d7cd85c512a
Chris Wand <chris.wand@gmail.com>
2015-01-26 12:02:28 -0500

    minor changes to how geocoding works

--- app/workers/geocoder_worker.rb
+++ app/workers/geocoder_worker.rb
@@ -3,7 +3,8 @@ class GeocoderWorker
 
   def perform(id)
     voter = Voter.find(id)
-    latlng = Geokit::Geocoders::MultiGeocoder.geocode voter.full_address
+    address = "#{voter.address1}, #{voter.city}, #{voter.state} #{voter.zip}"
+    latlng = Geokit::Geocoders::MultiGeocoder.geocode address
     voter.latitude = latlng.lat
     voter.longitude = latlng.lng
     voter.save
