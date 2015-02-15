0c409eb5136bccfc8d51f240adbe9ceb9ba461f1
Chris Wand <chris.wand@gmail.com>
2015-02-15 11:33:14 -0500

    Added unit tests for methods on Voter model

--- spec/models/voter_spec.rb
+++ spec/models/voter_spec.rb
@@ -18,3 +18,22 @@ RSpec.describe Voter, ".full_name" do
     expect(voter.full_name).to eq voter.first_name + " " + voter.last_name
   end
 end
+
+RSpec.describe Voter, ".full_address" do
+  it "returns full address, including city, state and zip" do
+    voter = FactoryGirl.create(:voter)
+    expect(voter.full_address).to eq(
+    "#{voter.address1} #{voter.address2}, #{voter.city}, #{voter.state} #{voter.zip}"
+    )
+  end
+end
+
+RSpec.describe Voter, ".valid_coords?" do
+  it "checks if the voter has floats for lat and long" do
+    voter = FactoryGirl.create(:voter)
+    expect(voter.valid_coords?).to eq true
+    voter.latitude = nil
+    voter.longitude = nil
+    expect(voter.valid_coords?).to eq false
+  end
+end
